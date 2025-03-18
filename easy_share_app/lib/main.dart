fb730bc1797207829200c9ef8dc981d6c578e658
hacjj <84962881+hacjj@users.noreply.github.com>
2025-03-19 02:48:00 +0530

    Update main.dart

--- easy_share_app/lib/main.dart
+++ easy_share_app/lib/main.dart
@@ -229,6 +229,7 @@ class PublishPage extends StatelessWidget {
 class InboxPage extends StatelessWidget {
   const InboxPage({super.key});
 
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
